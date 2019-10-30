5511539b1f0749213d65e74a1c8e4bd434e888c2
thomas hastings <thomas@thomashastings.com>
2019-10-30 14:34:56 -0600

    updated db directory

--- api/auth/auth.go
+++ api/auth/auth.go
@@ -13,8 +13,8 @@ var mySigningKey = []byte("pleasedonthackmebro")
 func IsAuthorized(endpoint func(http.ResponseWriter, *http.Request)) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		if r.Header["Token"] != nil {
-			token, err := jwt.Parse(r.Header["Token"][0], func(token *jwt.Token) (interface{}, error) {
+		if r.Header["Authorization"] != nil {
+			token, err := jwt.Parse(r.Header["Authorization"][0], func(token *jwt.Token) (interface{}, error) {
 				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 					return nil, fmt.Errorf("There was an error")
 				}
